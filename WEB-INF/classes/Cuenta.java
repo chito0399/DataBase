953d7d350fc55bf8d4d365fc941d693b8da485ea
chito0399 <A01338798@itesm.mx>
2019-04-21 12:35:11 -0500

    setter cuenta

--- WEB-INF/classes/Cuenta.java
+++ WEB-INF/classes/Cuenta.java
@@ -1,9 +1,24 @@
 public class Cuenta{
-    public int username;
-    public String password;
+    private int username;
+    private String password;
 
     public Cuenta (int username, String password){
         this.username = username; 
         this.password = password;
     }
+
+    public void setUsername(int username){
+        this.username = username;
+    }
+
+    public int getUsername(){
+        return username;
+    }
+
+    public void setPassword(String password){
+        this.password = password;
+    }
+    public String getPassword(){
+        return password;
+    } 
 }
